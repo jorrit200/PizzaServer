c0705a4bcece5b537a0e326924cef2c304175582
jorrit <jorritdehaan200@gmail.com>
2024-03-13 13:33:36 +0100

    pizza

---
+++ PizzaServer/PizzaServer.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace PizzaServer;
+
+internal class PizzaServer
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
