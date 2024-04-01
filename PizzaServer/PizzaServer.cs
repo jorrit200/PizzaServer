1dd0da48fa72067896d96531b3cd8c99038bb7a9
jorrit <jorritdehaan200@gmail.com>
2024-04-01 15:26:53 +0200

    tcp server setup

--- PizzaServer/PizzaServer.cs
+++ PizzaServer/PizzaServer.cs
@@ -1,11 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Net;
+
 namespace PizzaServer;
+
+using System.Net.Sockets;
 
 internal class PizzaServer
 {
     public static void Main(string[] args)
     {
+        TcpListener tcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"),6789);
+        tcpListener.Start();
+        Console.WriteLine("server started");
+        TcpClient tcpClient = tcpListener.AcceptTcpClient();
+        Console.WriteLine("client connected");
         Console.WriteLine("Hello, World!");
     }
 }
